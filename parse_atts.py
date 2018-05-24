d1c80cbfd27d73051ee55eb97962348a3a4309a7
Cecilia <cema3961@colorado.edu>
2018-05-24 14:42:25 -0600

    Make sure that attparser is reset before any calls

--- parse_atts.py
+++ parse_atts.py
@@ -22,63 +22,63 @@ from pyutils.attparser import cocoParser, clefParser
 # set nltk data path
 import nltk
 
+
 def analyze(sents):
-	# do some statistics
-	usage = {'r1': 0, 'r2': 0, 'r3': 0, 'r4': 0, 'r5': 0, 'r6': 0, 'r7': 0, 'r8': 0}
-	for sent in sents:
-		for r in usage:
-			usage[r] = usage[r] + 1 if sent['atts'][r] != ['none'] else usage[r]
-	for r in ['r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8']:
-		usage[r] /= float(len(sents))
-		print('Usage of %s is %.2f%%.' % (r, usage[r] * 100))
+    # do some statistics
+    usage = {'r1': 0, 'r2': 0, 'r3': 0, 'r4': 0, 'r5': 0, 'r6': 0, 'r7': 0, 'r8': 0}
+    for sent in sents:
+        for r in usage:
+            usage[r] = usage[r] + 1 if sent['atts'][r] != ['none'] else usage[r]
+    for r in ['r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8']:
+        usage[r] /= float(len(sents))
+        print('Usage of %s is %.2f%%.' % (r, usage[r] * 100))
+
 
 def main(params):
+    dataset_splitBy = params['dataset'] + '_' + params['splitBy']
+    if not osp.isdir('cache/parsed_atts/' + dataset_splitBy):
+        os.makedirs('cache/parsed_atts/' + dataset_splitBy)
 
-	dataset_splitBy = params['dataset'] + '_' + params['splitBy']
-	if not osp.isdir('cache/parsed_atts/' + dataset_splitBy):
-		os.makedirs('cache/parsed_atts/' + dataset_splitBy)
+    # load parsed sents, where sents.json =
+    # [{sent_id, sent, parse, raw, tokens}], where parse = {dependencies, parsetree, text, workds}
+    path_to_parsed_sents = osp.join('cache/parsed_sents', dataset_splitBy, 'sents.json')
+    sents = json.load(open(path_to_parsed_sents))
 
-	# load parsed sents, where sents.json =
-	# [{sent_id, sent, parse, raw, tokens}], where parse = {dependencies, parsetree, text, workds}
-	path_to_parsed_sents = osp.join('cache/parsed_sents', dataset_splitBy, 'sents.json')
-	sents = json.load(open(path_to_parsed_sents))
+    # parse attributes for each sent
+    if 'refcoco' in params['dataset'] or 'sunspot' in params['dataset']:
+        attparser = cocoParser.CocoParser()
+    elif 'refclef' in params['dataset']:
+        attparser = clefParser.ClefParser()
 
-	# parse attributes for each sent
-	if 'refcoco' in params['dataset'] or 'sunspot' in params['dataset']:
-		attparser = cocoParser.CocoParser()
-	elif 'refclef' in params['dataset']:
-		attparser = clefParser.ClefParser()
+    for i, sent in enumerate(sents):
+        parse = sent['parse']
+        attparser.reset(parse)
 
-	for i, sent in enumerate(sents):
-		parse = sent['parse']
-		try:
-			attparser.reset(parse)
-			sent['atts'] = attparser.decompose() # return list of atts, i.e., {r1: [man], r2: [blue], r3: [], ...}
-			sent['left'] = attparser.leftWords() # return list of (wd, pos), excluding stopping words
-		except:
-			sent['atts'] = {'r1': ['none'], 'r2': ['none'], 'r3': ['none'], 'r4': ['none'], 'r5': ['none'],
-			'r6': ['none'], 'r7': ['none'], 'r8': ['none']}
-			sent['left'] = attparser.leftWords()
-		if i % 100 == 0:
-			print('%s/%s has been decomposed into attributes r1-r8.' % (i+1, len(sents)))
+        try:
+            sent['atts'] = attparser.decompose()  # return list of atts, i.e., {r1: [man], r2: [blue], r3: [], ...}
+        except:
+            sent['atts'] = {'r1': ['none'], 'r2': ['none'], 'r3': ['none'], 'r4': ['none'], 'r5': ['none'],
+                            'r6': ['none'], 'r7': ['none'], 'r8': ['none']}
 
-	# analyze
-	analyze(sents)
+        sent['left'] = attparser.leftWords()  # return list of (wd, pos), excluding stopping words
+        if i % 100 == 0:
+            print('%s/%s has been decomposed into attributes r1-r8.' % (i + 1, len(sents)))
 
-	# save
-	with open(osp.join('cache/parsed_atts/', dataset_splitBy, 'sents.json'), 'w') as io:
-		json.dump(sents, io)
+    # analyze
+    analyze(sents)
+
+    # save
+    with open(osp.join('cache/parsed_atts/', dataset_splitBy, 'sents.json'), 'w') as io:
+        json.dump(sents, io)
 
 
 if __name__ == '__main__':
+    # input
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', default='refcoco', help='dataset name')
+    parser.add_argument('--splitBy', default='unc', help='split By')
+    args = parser.parse_args()
+    params = vars(args)
 
-	# input
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--dataset', default='refcoco', help='dataset name')
-	parser.add_argument('--splitBy', default='unc', help='split By')
-	args = parser.parse_args()
-	params = vars(args)
-
-	# main
-	main(params)
-
+    # main
+    main(params)
